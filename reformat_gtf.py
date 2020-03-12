0ac5180be098be388866fd7d15878a106039dcf8
fairliereese <fairliek@comcast.net>
2020-03-12 14:48:21 -0400

    maybe works noW???

--- reformat_gtf.py
+++ reformat_gtf.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+from collections import defaultdict
 
 def get_args():
 
@@ -45,36 +46,6 @@ def get_field_value(key, fields):
     else:
         return fields.split(key+' "')[1].split()[0].replace('";','')
 
-def construct_new_entry(prev_line, coords, entry_type):
-
-	# print('Constructing new {} entry'.format(entry_type))
-	
-	# add gene or transcript type, coords, and len
-	prev_line[2] = entry_type
-	prev_line[3] = min(coords)
-	prev_line[4] = max(coords)
-	prev_line[7] = '.'
-
-	# change the fields to reflect what type we are now
-	new_fields = ''
-	fields = prev_line[-1]
-	gid = get_field_value('gene_id', fields)
-	new_fields += 'gene_id "{}";'.format(gid)
-
-	# if there's a gene name add it too
-	gname = get_field_value('gene_name', fields)
-	if gname:
-		new_fields += 'gene_name "{}";'.format(gname)
-
-	if entry_type == 'transcript':
-		tid = get_field_value('transcript_id', fields)
-		new_fields += ' transcript_id "{}";'.format(tid)
-		
-	prev_line[-1] = new_fields
-	prev_line = format_to_write(prev_line)
-
-	return prev_line
-
 def make_ofile_name(matfile, prefix=None):
 	fname = matfile.split('.gtf')[0]
 	if prefix:
@@ -85,6 +56,26 @@ def make_ofile_name(matfile, prefix=None):
 
 def format_to_write(line): 
 	return ''.join('\t'.join([str(i) for i in line])+'\n')
+
+def construct_new_entry(entry, entry_type, min_coord, max_coord):
+
+	entry.type = entry_type
+	fields = ''
+	gid = entry.gid.tolist()[0]
+	gname = entry.gname.tolist()[0]
+
+	entry.start = min_coord
+	entry.stop = max_coord
+
+	if entry_type == 'gene' or entry_type == 'transcript':
+		fields += 'gene_id "{}";'.format(gid)
+		fields += ' gene_name "{}";'.format(gname)
+	if entry_type == 'transcript':
+		tid = entry.tid.tolist()[0]
+		fields += ' transcript_id "{}";'.format(tid)
+	entry.fields = fields
+
+	return entry
 
 def main():
 	args = get_args()
@@ -97,120 +88,77 @@ def main():
 		print('GTF has both gene and transcript entries. Nothing to add.')
 		return
 
-	# loop through this thing
-	infile = open(gtffile, 'r')
-	outfile = open(make_ofile_name(gtffile), 'w')
+	outfile = make_ofile_name(gtffile)
 
-	curr_gid = ''
-	curr_gid_coords = []
+	df = pd.read_csv(gtffile, sep='\t',
+		names=['chr', 'source', 'type', 'start', 'stop',
+		'score', 'strand', 'phase', 'fields'])
 
-	curr_tid = ''
-	curr_tid_coords = []
+	df['tid'] = df.apply(lambda x: get_field_value('transcript_id', x.fields), axis=1)
+	df['gid'] = df.apply(lambda x: get_field_value('gene_id', x.fields), axis=1)
+	df['gname'] = df.apply(lambda x: get_field_value('db_xref', x.fields), axis=1)
 
-	first_transcript = True
-	first_exon = True
+	gene_mins = df[['gid', 'start']].groupby(by='gid').min().copy(deep=True)
+	gene_maxes = df[['gid', 'stop']].groupby(by='gid').max().copy(deep=True)
 
-	gene_list = []
-	transcript_list = []
+	transcript_mins = df[['tid', 'start']].groupby(by='tid').min().copy(deep=True)
+	transcript_maxes = df[['tid', 'stop']].groupby(by='tid').max().copy(deep=True)
 
-	prev_line = ''
+	for gid in df.gid.unique():
 
-	# relevant entries
-	entries = ['exon']
+		if gid == None:
+			continue
 
-	if missing_gene: 
-		entries.append('transcript')
+		# if there's already an entry
+		if len(df.loc[(df.gid == gid) & (df.type == 'gene')].index) != 0:
+			pass
 
-	if missing_gene or missing_transcript:
+		# construct gene entry from its exons
+		else: 
+			g_min = gene_mins.loc[gid, 'start'].tolist()[0]
+			g_max = gene_maxes.loc[gid, 'stop'].tolist()[0]
 
-		for line in infile: 
+			# pull info out of constituent transcripts
+			g_entry = df.loc[df.gid == gid].head(1).copy(deep=True)
+			g_entry = construct_new_entry(g_entry, 'gene', g_min, g_max)
 
-			# skip the dumb header lines
-			if '##' in line or line.startswith('#'):
+			# and add new entry into the df
+			new_loc = entry.index.tolist()[0]
+			df_new = pd.concat([df.iloc[:new_loc], t_entry, df.iloc[new_loc:]]).reset_index(drop=True)
+			df = df_new
+
+		for tid in df.tid.unique():
+
+			if gid == None or tid == None: 
 				continue
 
-			line = line.strip().split('\t')
-			fields = line[-1]
+			# if there's already an entry 
+			if len(df.loc[(df.tid == tid) & (df.type == 'transcript')].index) != 0:
+				pass
 
-			gid = get_field_value('gene_id', fields)
-			tid = get_field_value('transcript_id', fields)
+			# construct transcript entry from its exons
+			else:
 
-			if line[2] in entries:
+				print(tid)
 
-				# set variables if first entry
-				if first_exon: 
-					curr_gid = gid
-					curr_tid = tid
+				t_min = transcript_mins.loc[tid].tolist()[0]
+				t_max = transcript_maxes.loc[tid].tolist()[0]
 
-					curr_gid_coords = [int(line[3]), int(line[4])]
-					curr_tid_coords = [int(line[3]), int(line[4])]
+				print('min: {}'.format(t_min))
+				print('max: {}'.format(t_max))
 
-					first_exon = False
+				# pull info out of constituent exons
+				t_entry = df.loc[df.tid == tid].head(1).copy(deep=True)
+				t_entry = construct_new_entry(t_entry, 'transcript', t_min, t_max)
+				print()
 
-					prev_line = line
+				# and add new entry into the df
+				new_loc = t_entry.index.tolist()[0]
+				df_new = pd.concat([df.iloc[:new_loc], t_entry, df.iloc[new_loc:]]).reset_index(drop=True)
+				df = df_new
 
-				# found a new transcript
-				elif missing_transcript and tid != curr_tid: 
+	df.to_csv(outfile, sep='\t', header=False)
 
-					# create transcript entry and dump to current gene list
-					new_entry = construct_new_entry(
-						prev_line, curr_tid_coords, 'transcript')
-					transcript_list = new_entry+''.join(transcript_list) 
+if __name__ == '__main__': main()
 
-					gene_list += transcript_list 
-					transcript_list = ''
-					curr_tid_coords = []
-
-				if missing_gene and gid != curr_gid:
-					
-					# create gene entry and write current gene list
-					new_entry = construct_new_entry(
-						prev_line, curr_gid_coords, 'gene')
-					gene_list = new_entry+''.join(gene_list)
-
-					gene_list += ''.join(transcript_list)
-					transcript_list = ''
-					curr_tid_coords = []
-
-					outfile.write(gene_list)
-					gene_list = ''
-					curr_gid_coords = []
-
-				# update loop vars
-				curr_gid = gid
-				curr_tid = tid
-				curr_gid_coords.append(int(line[3]))
-				curr_gid_coords.append(int(line[4]))
-				curr_tid_coords.append(int(line[3]))
-				curr_tid_coords.append(int(line[4]))
-
-				prev_line = line
-
-			# regardless, append to list of entries to write
-			transcript_list += format_to_write(line)
-
-		# if we've reached the end of the file
-		# create transcript entry and dump to current gene list
-		if missing_transcript:
-			new_entry = construct_new_entry(
-				prev_line, curr_tid_coords, 'transcript')
-			transcript_list = new_entry+''.join(transcript_list)
-
-		gene_list += transcript_list 
-		transcript_list = ''
-
-		# create gene entry and write current gene list
-		if missing_gene:
-			new_entry = construct_new_entry(
-				prev_line, curr_gid_coords, 'gene')
-		
-		gene_list = new_entry+''.join(gene_list)
-		outfile.write(gene_list)
-		gene_list = ''
-
-
-		infile.close()
-		outfile.close()
-
-if __name__ == '__main__':
-	main()
+			
